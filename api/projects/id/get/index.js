bfa08160e824d74e43b0234056771d31fb210e47
Maciej Gibas <gibasmaciej@gmail.com>
2017-02-27 07:56:21 -0500

    GET Projects/id

---
+++ api/projects/id/get/index.js
@@ -0,0 +1,21 @@
+'use strict';
+// const aws = require('aws-sdk');
+exports.get = (event, context, callback) => {
+  console.log(event);
+  // let s3 = new aws.S3();
+  // let bucket = event.stageVariables.BucketName;
+  // s3.getObject({
+  //   Bucket: bucket,
+  //   Key: `users/${event.requestContext.authorizer.principalId}/profile.json`
+  // }, (err, data) => {
+  //   if (err) {
+  //     console.log(err);
+  //     callback('There was an error.');
+  //   } else {
+  //     callback(null, {
+  //       statusCode: 200,
+  //       body: data.Body.toString()
+  //     });
+  //   }
+  // });
+};
