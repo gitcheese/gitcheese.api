2b157329ccd70a2d6ab55fe98ccf1c2aa33e7656
Maciej Gibas <gibasmaciej@gmail.com>
2017-02-27 08:01:07 -0500

    Removed authorizer for now

--- api/projects/id/get/index.js
+++ api/projects/id/get/index.js
@@ -1,21 +1,21 @@
 'use strict';
-// const aws = require('aws-sdk');
+const aws = require('aws-sdk');
 exports.get = (event, context, callback) => {
-  console.log(event);
-  // let s3 = new aws.S3();
-  // let bucket = event.stageVariables.BucketName;
-  // s3.getObject({
-  //   Bucket: bucket,
-  //   Key: `users/${event.requestContext.authorizer.principalId}/profile.json`
-  // }, (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     callback('There was an error.');
-  //   } else {
-  //     callback(null, {
-  //       statusCode: 200,
-  //       body: data.Body.toString()
-  //     });
-  //   }
-  // });
+  let s3 = new aws.S3();
+  let bucket = event.stageVariables.BucketName;
+  let id = event.parameters.id;
+  s3.getObject({
+    Bucket: bucket,
+    Key: `users/${event.requestContext.authorizer.principalId}/repos/${id}/data.json`
+  }, (err, data) => {
+    if (err) {
+      console.log(err);
+      callback('There was an error.');
+    } else {
+      callback(null, {
+        statusCode: 200,
+        body: data.Body.toString()
+      });
+    }
+  });
 };
