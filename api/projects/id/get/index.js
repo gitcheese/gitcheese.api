8514838d1453a8dcfdd3b740b119025fbe71d8a2
Maciej Gibas <gibasmaciej@gmail.com>
2017-02-27 08:06:07 -0500

    Added authorizer back

--- api/projects/id/get/index.js
+++ api/projects/id/get/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const aws = require('aws-sdk');
 exports.get = (event, context, callback) => {
+  console.log(event);
   let s3 = new aws.S3();
   let bucket = event.stageVariables.BucketName;
   let id = event.parameters.id;
