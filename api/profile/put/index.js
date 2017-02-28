5e1762aad670fc7dba5b30e3335808e0e62f711f
Maciej Gibas <gibasmaciej@gmail.com>
2017-02-27 20:50:19 -0500

    PUT profile

---
+++ api/profile/put/index.js
@@ -0,0 +1,21 @@
+'use strict';
+const aws = require('aws-sdk');
+exports.put = (event, context, callback) => {
+  let s3 = new aws.S3();
+  let bucket = event.stageVariables.BucketName;
+  let userId = event.requestContext.authorizer.principalId;
+  s3.putObject({
+    Bucket: bucket,
+    Key: `users/${userId}/profile.json`,
+    Body: JSON.stringify(event.body)
+  }, (err, data) => {
+    if (err) {
+      console.log(err);
+      callback('There was an error.');
+    } else {
+      callback(null, {
+        statusCode: 200
+      });
+    }
+  });
+};
