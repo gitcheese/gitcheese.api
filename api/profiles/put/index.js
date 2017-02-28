ec90b2574572cfb05b9468aa1d0914ef7b4c532e
Maciej Gibas <gibasmaciej@gmail.com>
2017-02-28 18:57:02 -0500

    Added validation for profile

--- api/profiles/put/index.js
+++ api/profiles/put/index.js
@@ -1,9 +1,20 @@
 'use strict';
 const aws = require('aws-sdk');
+const Validator = require('validatorjs');
 exports.put = (event, context, callback) => {
   let s3 = new aws.S3();
   let bucket = event.stageVariables.BucketName;
   let userId = event.requestContext.authorizer.principalId;
+  let rules = {
+    email: 'required|email',
+    country: 'required'
+  };
+  let data = JSON.parse(event.body);
+  var validation = new Validator(data, rules);
+  if (validation.fails()) {
+    callback(null, { statusCode: 400, body: validation.all() });
+    return;
+  }
   s3.putObject({
     Bucket: bucket,
     Key: `users/${userId}/profile.json`,
