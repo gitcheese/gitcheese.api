c2b6794f23c1b74b0def5938e3105e6cfdd16aa5
Maciej Gibas <gibasmaciej@gmail.com>
2017-03-07 18:24:02 -0500

    trouble shooting login

--- api/tokens/github/callback/get/index.js
+++ api/tokens/github/callback/get/index.js
@@ -104,14 +104,9 @@ exports.get = (event, context, callback) => {
       }
     })
     .then((userProfile) => {
-      console.log('finalizing');
       let token = jwt.sign(userProfile, event.stageVariables.JWTSecret);
       let url = `${event.stageVariables.RedirectUrl}?token=${token}`;
-      console.log({
-        statusCode: 302,
-        headers: { location: url }
-      });
-      callback(null, {
+      return callback(null, {
         statusCode: 302,
         headers: { location: url }
       });
