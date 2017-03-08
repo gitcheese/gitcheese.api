58064d583b13d721ba0189717866dec05a1eb937
Maciej Gibas <gibasmaciej@gmail.com>
2017-03-08 13:59:39 -0500

    Fixed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,16 @@ const uglify = require('gulp-uglify');
 const replace = require('gulp-token-replace');
 gulp.task('default', () => {
   gulp.src('api/**/package.json')
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist/api'))
     .pipe(install());
-  gulp.src(['api/**/*.js', 'aws-api-gateway/*.js', '!node_modules/**'])
+  gulp.src(['api/**/*.js', '!node_modules/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/api/'));
   gulp.src('cloudformation/**/*')
     .pipe(replace({ global: process.env }))
     .pipe(gulp.dest('dist/cloudformation'));
