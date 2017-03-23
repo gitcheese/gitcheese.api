cac036f72340c5d847502ad2d78b0c1416f291c2
Maciej Gibas <gibasmaciej@gmail.com>
2017-03-23 13:55:33 -0400

    Debugging handler

--- async-handlers/donation-created-handler/index.js
+++ async-handlers/donation-created-handler/index.js
@@ -2,4 +2,11 @@
 // const aws = require('aws-sdk');
 exports.handler = (event, context, callback) => {
   console.log(event.Records[0].s3);
+  let bucket = event.Records[0].s3.bucket.name;
+  let repoKey = event.Records[0].s3.object.key
+    .split('/')
+    .splice(-1, 3)
+    .join('/');
+  console.log(bucket);
+  console.log(repoKey);
 };
