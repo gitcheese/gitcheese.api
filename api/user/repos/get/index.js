b3cb1d23fadfdb24b4600d5fdbf5401ad14eecd2
Maciej Gibas <gibasmaciej@gmail.com>
2017-03-24 13:28:55 -0400

    Using list.json to return repos

--- api/user/repos/get/index.js
+++ api/user/repos/get/index.js
@@ -4,33 +4,14 @@ const http = require('api-utils').http;
 exports.get = (event, context, callback) => {
   let s3 = new aws.S3();
   let bucket = event.stageVariables.BucketName;
-  let userId = event.requestContext.authorizer.principalId;
-  s3.listObjectsV2({
+  s3.getObject({
     Bucket: bucket,
-    Prefix: `users/${userId}/repos`
+    Key: `users/${event.requestContext.authorizer.principalId}/repos/list.json`
   }, (err, data) => {
     if (err) {
-      console.log(err);
-      return http.response.error(callback);
+      return http.response.ok(callback, []);
     } else {
-      let projectPromises = data.Contents.map(c => {
-        return new Promise((resolve, reject) => {
-          s3.getObject({
-            Bucket: bucket,
-            Key: c.Key
-          }, (err, data) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(JSON.parse(data.Body.toString()));
-            }
-          });
-        });
-      });
-      Promise.all(projectPromises)
-        .then(projects => {
-          return http.response.ok(callback, projects);
-        });
+      return http.response.ok(callback, JSON.parse(data.Body.toString()));
     }
   });
 };
